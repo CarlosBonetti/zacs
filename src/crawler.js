b1620eec0555a068b85cbe61fd89b836e0e375b9
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-06-01 18:41:22 -0300

    Remove puppeteer. Using simple requests to retrieve zeplin api data

--- src/crawler.js
+++ src/crawler.js
@@ -1,92 +1,35 @@
-import puppeteer from 'puppeteer'
-import pLimit from 'p-limit'
+import request from 'request-promise-native'
 
 import config from './config'
-import logger from './logger'
 
-/**
- * Retrieve all artboard images from a zeplin project.
- *
- * @param projectId The ID of the project to retrieve images from.
- * @return A map of screenIds and its PNG image as value.
- */
-const getArtboardImages = async (projectId) => {
-    const result = {}
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    page.setDefaultNavigationTimeout(config.CRAWLER_TIMEOUT)
-
-    // Login
-    await page.goto(`https://app.zeplin.io/project/${projectId}}`)
-    await page.type('[name=handle]', config.ZEPLIN_USERNAME)
-    await page.type('[name=password]', config.ZEPLIN_PASSWORD)
-    await page.click('button')
-    await page.waitForNavigation()
-
-    // Get screen ids
-    const screens = await page.$$eval('.screen', (elems) => elems.map(elem => elem.getAttribute('data-id')))
-    logger.debug(`Found ${screens.length} screens`)
-    await page.close()
-
-    // Get screen IDs with multiple tabs, limiting by a number of concurrent tabs
-    const limit = pLimit(4)
-    const jobs = screens.map(sid => limit(() => {
-        return getArtboardImage(browser, projectId, sid)
-            .then(src => result[sid] = src)
-    }))
-    await Promise.all(jobs)
-
-    await browser.close()
-    return result
+const requestLogin = async () => {
+    return await request.post('https://api.zeplin.io/users/login', {
+        form: {
+            handle: config.ZEPLIN_USERNAME,
+            password: config.ZEPLIN_PASSWORD,
+        }
+    })
 }
 
-/**
- * Retrieve a single screen image.
- *
- * @param browser A puppeteer browser instance already logged in.
- * @param projectId The ID of the projecto to retrieve the image from.
- * @param sid The screen ID.
- * @return The image URL of the screen.
- */
-const getArtboardImage = async (browser, projectId, sid) => {
-    const page = await browser.newPage()
-    page.setDefaultNavigationTimeout(config.CRAWLER_TIMEOUT)
+const requestApiData = async (authToken, projectId) => {
+    const body = await request.get(`https://app.zeplin.io/project/${projectId}`, {
+        headers: {
+            'Cookie': request.cookie(`userToken=${authToken}`)
+        }
+    })
 
-    await page.goto(`https://app.zeplin.io/project/${projectId}/screen/${sid}`)
-    await page.waitForSelector('#screenImage[src]')
-    const src = await page.$eval('#screenImage', (elem) => elem.getAttribute('src'))
-    if (src === null) {
-        await page.screenshot({ path: `screenshots/${sid}.png` })
+    const regex = /window\.Zeplin\[\"apiData\"\] = (.*)<\/script>/g
+    const res = regex.exec(body)
+
+    return eval(res[1]) // TODO: DANGER
+}
+
+export const getApiData = async (projectId) => {
+    const auth = JSON.parse(await requestLogin())
+    if (!auth.token) {
+        throw new Error('Auth token not found')
     }
-    await page.close()
-    logger.debug(sid + ': ' + src)
-    return src
-}
 
-/**
- * Retrieve a single artboard image from a zeplin project.
- *
- * @param projectId The project ID to retrieve the artboard image from.
- * @param sid The screen ID.
- * @return The image URL of the screen.
- */
-export const getScreen = async (projectId, sid) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    page.setDefaultNavigationTimeout(config.CRAWLER_TIMEOUT)
-
-    // Login
-    await page.goto(`https://app.zeplin.io/project/${projectId}`)
-    await page.type('[name=handle]', config.ZEPLIN_USERNAME)
-    await page.type('[name=password]', config.ZEPLIN_PASSWORD)
-    await page.click('button')
-    await page.waitForNavigation()
-
-    // Get image
-    await page.goto(`https://app.zeplin.io/project/${projectId}/screen/${sid}`)
-    await page.waitForSelector('#screenImage[src]')
-    const src = await page.$eval('#screenImage', (elem) => elem.getAttribute('src'))
-    await page.close()
-    await browser.close()
-    return src
+    const apiData = await requestApiData(auth.token, projectId)
+    return apiData
 }
--- src/service.js
+++ src/service.js
@@ -1,6 +1,22 @@
 import { Project, Screen } from './db'
-import { getScreen } from './crawler'
+import { getApiData } from './crawler'
 import logger from './logger'
+
+export const getScreen = async (projectId, screenId) => {
+    const apiData = await getApiData(projectId)
+
+    console.log(apiData)
+
+    console.log(apiData.project.screens)
+
+    for (const s of apiData.project.screens) {
+        console.log(s.latestVersion.snapshot.url)
+    }
+
+    return apiData.project.screens
+        .find(screen => screen._id === screenId)
+        .latestVersion.snapshot.url
+}
 
 export const getScreenUrl = async (projectId, screenId) => {
     let project = await Project.findOne({ pid: projectId })
