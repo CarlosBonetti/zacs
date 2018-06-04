8592dda98b8ed19936cc57838a46515183be6b8a
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-06-04 15:14:09 -0300

    Logging on console even on production

--- src/logger.js
+++ src/logger.js
@@ -10,19 +10,16 @@ const logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.Console({
+            level: 'debug',
+            format: format.combine(
+                format.colorize(),
+                format.align(),
+                format.timestamp(),
+                format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+            )
+        })
     ]
 })
-
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        level: 'debug',
-        format: format.combine(
-            format.colorize(),
-            format.align(),
-            format.timestamp(),
-            format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-        )
-    }))
-}
 
 export default logger
